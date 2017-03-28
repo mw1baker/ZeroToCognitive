ce90e5e29b71a8aa3c8810468ec2078e3fd5935a
mw1baker <blewkitty@gmail.com>
2017-03-27 22:41:24 -0700

    updated credentials

--- Chapter05/controller/restapi/features/classifier.js
+++ Chapter05/controller/restapi/features/classifier.js
@@ -23,13 +23,32 @@ var appEnv = cfenv.getAppEnv();
 
 var serviceCreds = appEnv.getServiceCreds(WATSON_NLC_SERVICE_NAME) || process.env.NLC_CREDS || config.watson_nlc;
 var natural_language_classifier = watson.natural_language_classifier(serviceCreds);
-var classifier_id_industry = process.env.NLC_CLASSIFIER_ID || "your classifier id goes inside these quote marks";
+var classifier_id_industry = process.env.NLC_CLASSIFIER_ID || 'f5bbbbx174-nlc-8563';
 exports.classifyInd = function(req, res) {
   console.log("Classifier entered");
-
+    _text = req.body.cquery;
+    i_output = {};
+    (function(_res) {
+        natural_language_classifier.classify({
+                text: _text,
+                classifier_id: classifier_id_industry
+            },
+            function(err, response) {
+                _res.writeHead(200, { "Content-Type": "text/plain" });
+                _res.end(nlc_res("Industry", err, response));
+            });
+    })(res)
 }
 
 function nlc_res(classifier, err, response) {
-
-
+    var _output = [];
+    if (err) {
+        console.log(classifier + ' error:', err);
+        _output = { 'error': JSON.stringify(err, null, 2) };
+    } else {
+        _output = { results: JSON.stringify(response, null, 2) };
+    }
+    console.log("Printing from nlc_res");
+    console.log(_output);
+    return (JSON.stringify(_output, null, 2));
 }
--- Chapter06/HTML/js/z2c-NLC.js
+++ Chapter06/HTML/js/z2c-NLC.js
@@ -13,52 +13,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// displayNLC results
-function checkNLC(_display, _source)
-{
-  var options = {};
-  options.cquery = _source[0].innerText;
-  $.when($.get(_display), $.post('/api/understand/classifyInd', options)).done(function(_page, _nlc_results){
-    console.log("page returned");
-    var _target= $("#modal");
-    _target.append(_page);
-    _target.height($(window).height());
-    _target.show();
-    _data = _nlc_results[0];
-    _classes = JSON.parse(JSON.parse(_data).results).classes;
-    displayNLC($("#industryResult"), _classes);
-    closeNLC=$("#close_NLC");
-    closeNLC.on("click", function(){
-      console.log("closeNLC clicked.")
-      $("#modal").empty();
-    });
-});
-}
+ // displayNLC results
+ var industryTable = "#industryResult";
+ var nlc_classes; var industryPage="displayNLC.html";
 
-function getIndustryClassification(_source, _string)
-{
+ function displayNLC(_target, _results)
+ {
+   var target = _target;
+   target.find("tr:not(:first)").remove();
+   indName = _results[0]["class_name"];
+   var len = _results.length;
+   _idx = 0;
+   while (_idx < len)
+   {
+     (function(_idx, data)
+       {  _cStr = data[_idx]["class_name"];
+         target.append("<tr><td style='width: 60%'>"+_cStr +"</td><td>"+data[_idx]["confidence"]+"</td></tr>");})
+       (_idx, _results)
+     _idx++;
+   }
+   target.append("</table>");
+ }
 
-}
+ function getIndustryClassification(_source, _string)
+ {
+   var options = {};
+   options.cquery = _source[0].innerText;
+   $.when( $.post('/api/understand/classifyInd', options)).done(function(_nlc_results){
+     _data = _nlc_results;
+     nlc_classes = JSON.parse(JSON.parse(_data).results).classes;
+     industry = nlc_classes[0].class_name;
+     toggle_mic(_mic, _stop, false)
+     talkToMe(dialog_target, _string.format(getCookieValue("name"), industry));
+   });
+ }
 
-function setModal(_display, cbfn, _modalTarget, _results)
-{
-
-}
-
-function displayNLC(_target, _results)
-{
-  var target = _target;
-  target.find("tr:not(:first)").remove();
-  indName = _results[0]["class_name"];
-  var len = _results.length;
-  _idx = 0;
-  while (_idx < len)
-  {
-    (function(_idx, data)
-      {  _cStr = data[_idx]["class_name"];
-        target.append("<tr><td style='width: 60%'>"+_cStr +"</td><td>"+data[_idx]["confidence"]+"</td></tr>");})
-      (_idx, _results)
-    _idx++;
-  }
-  target.append("</table>");
-}
+ function setModal(_display, cbfn, _modalTarget, _results)
+ {
+   $.when($.get(_display)).done(function(_page){
+     var _target= $("#modal");
+     _target.append(_page);
+     _target.height($(window).height());
+     _target.show();
+     closeNLC=$("#close_NLC");
+     closeNLC.on("click", function(){
+       $("#modal").empty();
+       nextStep();
+     });
+     cbfn($(_modalTarget), _results);
+   });
+ }
